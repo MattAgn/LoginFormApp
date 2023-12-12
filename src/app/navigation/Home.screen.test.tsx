1861a76a27fd02aff29801269ab950b443b3fa30
MattAgn <matthieua@bam.tech>
2023-12-12 09:35:23 +0100

    test: add tests

---
+++ src/app/navigation/Home.screen.test.tsx
@@ -0,0 +1,58 @@
+import { Home } from '#app/navigation/Home.screen';
+import { renderWithProviders } from '#testing/render';
+import {
+  fireEvent,
+  screen,
+  userEvent,
+  waitForElementToBeRemoved,
+} from '@testing-library/react-native';
+
+test('[OLD WAY] form completion with error', async () => {
+  renderWithProviders(<Home />);
+  const EmailInput = screen.getByLabelText('Email address');
+  const PasswordInput = screen.getByLabelText('Password');
+  const LoginButton = screen.getByRole('button', { name: 'Login' });
+
+  fireEvent.changeText(EmailInput, 'mat@gmailcom');
+  fireEvent.changeText(PasswordInput, 'azerty');
+  fireEvent.press(LoginButton);
+
+  expect(await screen.findByText('Invalid email address')).toBeOnTheScreen();
+
+  fireEvent(EmailInput, 'clear');
+  fireEvent.changeText(EmailInput, 'mat@gmail.com');
+  fireEvent(EmailInput, 'blur');
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText('Invalid email address'),
+  );
+
+  fireEvent.press(LoginButton);
+
+  expect(await screen.findByText('Hello Matt!')).toBeOnTheScreen();
+});
+
+test('[NEW WAY] form completion with error', async () => {
+  renderWithProviders(<Home />);
+  const EmailInput = screen.getByLabelText('Email address');
+  const PasswordInput = screen.getByLabelText('Password');
+  const LoginButton = screen.getByRole('button', { name: 'Login' });
+
+  userEvent.type(EmailInput, 'mat@gmailcom', {
+    submitEditing: true,
+  });
+  userEvent.type(PasswordInput, 'azerty', { submitEditing: true });
+
+  expect(await screen.findByText('Invalid email address')).toBeOnTheScreen();
+
+  userEvent.clear(EmailInput);
+  userEvent.type(EmailInput, 'mat@gmail.com');
+
+  await waitForElementToBeRemoved(() =>
+    screen.getByText('Invalid email address'),
+  );
+
+  userEvent.press(LoginButton);
+
+  expect(await screen.findByText('Hello Matt!')).toBeOnTheScreen();
+});
